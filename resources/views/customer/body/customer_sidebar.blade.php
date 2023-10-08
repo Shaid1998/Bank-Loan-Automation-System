299db49a2dba93fe9a1fa7d83145c969f1dbb531
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-10-08 18:14:28 +0600

    Frist commit

---
+++ resources/views/customer/body/customer_sidebar.blade.php
@@ -0,0 +1,58 @@
+
+<div style="background-image: linear-gradient(to bottom right, red, yellow);" class="sidebar-wrapper" data-simplebar="true">
+	@php
+		$id = Auth::user()->id;
+		$adminData = App\Models\User::find($id);
+	@endphp	
+	<div class="user-profile text-center mt-3">
+		<div class="">
+			<img src="{{$adminData->photo}}"  class="avatar-md rounded-circle">
+		</div>
+		<div class="mt-3">
+			<h4 style="color:rgb(0, 30, 255);font-size:40px;" class="font-size-16 mb-1">{{$adminData->username}}</h4>
+			<span style="color:rgb(0, 30, 255);font-size:20px;" class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i> Online</span>
+		</div>
+	</div>
+		<!--navigation-->
+	<div>
+		<ul class="metismenu" id="menu">
+			<li style="padding-top: 1rem">
+				<a href="{{ route('admin.dashobard') }}">
+					<div style="color:rgb(0, 30, 255);font-size:25px;" class="parent-icon"><i class='fa fa-home'></i></div>
+					<div style="color:rgb(0, 30, 255);font-size:16px;font-weight:500;" class="menu-title">DASHBOARD</div>
+				</a>
+			</li>
+			<li style="padding-top: 1rem">
+				<a href="{{route ('all.user.information')}}">
+					<div style="color:rgb(0, 30, 255);font-size:25px;" class="parent-icon"><i class="fas fa-user-check"></i></div>
+					<div style="color:rgb(0, 30, 255);font-size:16px;font-weight:500;" class="menu-title">USER INFO</div>
+				</a>
+			</li>
+			<li style="padding-top: 1rem">
+				<a href="{{route ('all.worker.information')}}">
+					<div style="color:rgb(0, 30, 255);font-size:25px;" class="parent-icon"><i class="fas fa-user-check"></i></div>
+					<div style="color:rgb(0, 30, 255);font-size:16px;font-weight:500;" class="menu-title">WORKER INFO</div>
+				</a>
+			</li>
+			<li style="padding-top: 1rem">
+				<a href="{{ route('all.incomeing.messages') }}">
+					<div style="color:rgb(0, 30, 255);font-size:25px;" class="parent-icon"><i class="fas fa-envelope"></i></div>
+					<div style="color:rgb(0, 30, 255);font-size:16px;font-weight:500;" class="menu-title">MESSAGES</div>
+				</a>
+			</li>
+			<li style="padding-top: 1rem">
+				<a href="{{route('admin.notice.board')}}">
+					<div style="color:rgb(0, 30, 255);font-size:25px;" class="parent-icon"><i class="fas fa-bell"></i></div>
+					<div style="color:rgb(0, 30, 255);font-size:16px;font-weight:500;" class="menu-title">NOTICE</div>
+				</a>
+			</li>
+			<li style="padding-top: 1rem">
+				<a href="{{route('admin.other.option')}}">
+					<div style="color:rgb(0, 30, 255);font-size:25px;" class="parent-icon"><i class="fas fa-bell"></i></div>
+					<div style="color:rgb(0, 30, 255);font-size:16px;font-weight:500;" class="menu-title">OTHERS</div>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<!--end navigation-->
+</div>
---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\CustomerController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+/// Admin
+
+Route::middleware(['auth','role:admin'])->group(function() {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashobard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+    Route::post('/admin/update/password', [AdminController::class, 'AdminUpdatePassword'])->name('update.password');
+});
+
+
+/// Employee
+
+Route::middleware(['auth','role:employee'])->group(function() {
+    Route::get('/employee/dashboard', [EmployeeController::class, 'EmployeeDashboard'])->name('employee.dashobard');
+    Route::get('/employee/logout', [EmployeeController::class, 'EmployeeDestroy'])->name('employee.logout');
+    Route::get('/employee/profile', [EmployeeController::class, 'EmployeeProfile'])->name('employee.profile');
+    Route::post('/employee/profile/store', [EmployeeController::class, 'EmployeeProfileStore'])->name('employee.profile.store');
+    Route::get('/employee/change/password', [EmployeeController::class, 'EmployeeChangePassword'])->name('employee.change.password');
+    Route::post('/employee/update/password', [EmployeeController::class, 'EmployeeUpdatePassword'])->name('uemployee.password');
+});
+
+/// Employee
+
+Route::middleware(['auth','role:customer'])->group(function() {
+    Route::get('/customer/dashboard', [CustomerController::class, 'CustomerDashboard'])->name('customer.dashobard');
+    Route::get('/customer/logout', [CustomerController::class, 'CustomerDestroy'])->name('customer.logout');
+    Route::get('/customer/profile', [CustomerController::class, 'CustomerProfile'])->name('customer.profile');
+    Route::post('/customer/profile/store', [CustomerController::class, 'CustomerProfileStore'])->name('customer.profile.store');
+    Route::get('/customer/change/password', [CustomerController::class, 'CustomerChangePassword'])->name('customer.change.password');
+    Route::post('/customer/update/password', [CustomerController::class, 'CustomerUpdatePassword'])->name('ucustomer.password');
+});
